7f3b1e2b89fad9e43b16c7581c1f192c2b3656d8
TeXmeijin <i8858garden@gmail.com>
2020-12-17 10:31:51 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# typesafe-layered-breadcrumbs
+
+## 概要
+
+型安全かつ、階層構造を保ったパンくずを実装できるライブラリです。
+
+<WIP>
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+type Dictionary<T> = { [key: string]: T }
+type RouteLocation = {
+  name?: string
+  path?: string
+  query?: Dictionary<string | (string | null)[] | null | undefined>
+  params?: Dictionary<string>
+}
+
+export type BreadcrumbsItem = {
+  // displayed text
+  readonly text?: string
+  // title attribute of anchor tags
+  readonly title?: string
+  // URL or Location Object
+  readonly to?: string | RouteLocation
+  // disable flag of link
+  disabled?: boolean
+}
+
+export type Breadcrumbs = BreadcrumbsItem[]
+
+export type ToArray<T> = T extends Array<unknown> ? T : [T]
+
+export type BuildBreadcrumbs<T = []> = (...args: ToArray<T>) => Breadcrumbs 
